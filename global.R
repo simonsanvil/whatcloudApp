f8df234fb6dc9e4a6b520af0ade47f5637ad2750
simonsanvil <simonsanvil2012@gmail.com>
2019-05-11 00:26:44 +0200

    Shiny app to visualize whatsapp conversations
    This is the code for a shiny app that creates a word cloud out of a .TXT file, it is optimized for the kind of texts that can be obtained out of a whatsapp chats.

---
+++ global.R
@@ -0,0 +1,84 @@
+if (!require(tm)){
+    install.packages('tm')}
+library(tm)
+
+if (!require(wordcloud)){
+    install.packages('wordcloud')}
+library(wordcloud)
+
+if (!require(memoise)){
+    install.packages('memoise')}
+library(memoise)
+
+if (!require(dplyr)){
+  install.packages('dplyr')}
+require(dplyr)
+
+if (!require(textclean)){
+  install.packages('textclean')}
+require(textclean)
+
+if (!require(stringr)){
+  install.packages('stringr')}
+require(stringr)
+
+# Using "memoise" to automatically cache the results
+getChat <- function(f,type) {
+  # Careful not to let just any name slip in here; a
+  # malicious user could manipulate this value.
+  
+  if(type == T){
+      tryCatch(text <- readLines(f$datapath,encoding = "UTF-8"), finally = text <- readLines(f$datapath,encoding = "UTF=8"))
+      }else{text <- readLines(f$datapath,encoding = "UTF=8")}
+  text <- replace_non_ascii(text)
+  text <- add_comma_space(text)
+  text <- replace_email(text, replacement = '')
+  text <- replace_url(text, replacement = '')
+  
+  
+  len = length(text)
+  
+  if(type == T){reg <- ".*?:\\s(.*)"}else{reg <- c(' ')}
+  txtdf = character(0)
+  
+  if(type == T){
+      for(i in 1:len){
+          m <- str_match(text[i], reg)
+          txtdf <- c(txtdf, m[2])
+      }
+  }else{for(i in 1:len){txtdf[i] <- c(text[i])}}
+  txtdf <- as.data.frame(txtdf)
+  colnames(txtdf) <- 'chat'
+  txtdf <- na.omit(txtdf)
+  
+  myCorpus = Corpus(VectorSource(txtdf$chat))
+  
+  suppressWarnings({
+    myCorpus <- tm_map(myCorpus, removePunctuation)
+    myCorpus <- tm_map(myCorpus, removeWords, stopwords('spanish'))
+    myCorpus <- tm_map(myCorpus, removeWords, stopwords('english'))
+    myCorpus <- tm_map(myCorpus, removeWords, c("media", "omitted",'this','deleted','message','voice','missed','call','llamada','perdida','voz','Missed','video'))
+    myCorpus <- tm_map(myCorpus, function(x) removeWords(x,c('media','que')))
+    myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+    myCorpus <- tm_map(myCorpus, removeNumbers)
+    myCorpus <- tm_map(myCorpus, removeWords,
+                      c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but",'dy','cent','si'))
+    })
+  
+  myDTM = TermDocumentMatrix(myCorpus,
+                             control = list(minWordLength = 1,wordLengths=c(0,Inf)))
+  
+  #txt = as.matrix(myDTM)
+  
+  freq_terms <- findFreqTerms(myDTM, 4)
+  if(length(freq_terms)>0){
+    
+    txt.m <- as.matrix(myDTM[freq_terms,])
+    # Create a dataframe with each word and it's frequency:
+    txt <- as.matrix(txt.m)
+  }else{txt <- as.matrix(myDTM)}
+  
+  txt = sort(rowSums(txt), decreasing = TRUE)
+  
+}
+
---
+++ server.R
@@ -0,0 +1,65 @@
+library(shiny)
+
+if (!require(wordcloud)){
+  install.packages('wordcloud')}
+library(wordcloud)
+
+shinyServer(function(input, output, session) {
+  # Define a reactive expression for the document term matrix
+
+  data <- reactive({
+    validate(
+      need(input$text != '', "Please introduce a file")
+    )
+  })
+
+  terms <- reactive({
+    # Change when the "update" button is pressed...
+
+    input$goButton
+
+    # ...but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Processing corpus...")
+        if(input$other == 'Yes'){getChat(input$text,T)}
+         else{getChat(input$text,F)}
+      })
+    })
+  })
+  
+  col <- reactive({
+      #input$goButton
+      input$background.color
+  })
+  
+  observe({
+  col <- col()
+  output$plot <- renderPlot({
+    data()
+    v <- terms()
+    v <- na.exclude(v)
+    opt <- input$palette
+    if(opt == 1){pal = brewer.pal(8, "Dark2")
+    }else if(opt == 2){pal = brewer.pal(8,"Paired")
+    }else if(opt == 3){pal = rev(rev(brewer.pal(8,'Greys'))[c(1,2,3,4)])
+    }else if(opt == 4){pal = rev(rev(brewer.pal(8,'Blues'))[c(0,1,2,3,4,5)])
+    }else if(opt == 5){pal = rev(rev(brewer.pal(8,'Oranges'))[c(0,1,2,3,4,5)])
+    }else{ pal = brewer.pal(8,'Set3')
+    }
+    par(mar = rep(0, 4))
+    set.seed(floor(runif(1, 1,301)))
+    wordcloud(names(v), v, #scale=c(4,0.5),
+                  min.freq = input$min.freq,
+                  max.words=input$max.words,
+                  colors=pal,
+                  random.order = F,
+                  rot.per = .3, main = 'title')
+    if (input$name == 'Yes'){
+        text(0.5,0.006,substr(input$text$name,0,nchar(input$text$name)-4),col = input$textCol)
+        }
+  },res = 90, bg = col) # #bg.col
+  })
+})
+
+
